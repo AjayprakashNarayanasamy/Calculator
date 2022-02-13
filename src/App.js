a4aa4b0c59b6492da712865138a10bc26b5cebf5
AjayprakashNarayanasamy <ajayprakashn66@gmail.com>
2022-02-13 14:23:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useReducer,useState } from 'react';
 import './App.css';
+import {reducer} from "./reducer"
 
 function App() {
+ 
+const [values1,setValues]=React.useState({
+  count:0,
+  num1:0,
+  num2:0
+})
+
+  
+  
+  const [state, dispatch] = useReducer(reducer, values1);
+
+
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3> Calculator  </h3>
+    <form onSubmit={(e) => handleSubmit(e)}>
+      Input value1 <input type="text" onChange={(e)=>{setValues(state.num1=(e.target.value))}} name="num1" /><br/><br/>
+      Input value2<input type="text" onChange={(e)=>{setValues(state.num2=e.target.value)}} name="num2"/><br/><br/>
+      <div>
+      <button onClick={() => dispatch({ type: 'add',  })}>
+        
+        Addition +
+      </button>
+      &nbsp;
+      <button onClick={() => dispatch({ type: 'sub',payload:{num1:state.num1, num2:state.num2} })}>
+       
+      Subtraction -
+      </button>
+       </div>
+        <br></br>
+      <div>
+      <button onClick={() => dispatch({ type: 'div' })}>
+        {' '}
+        Division /
+      </button> &nbsp;
+      <button onClick={() => dispatch({ type: 'mul' })}>
+        {' '}
+        Multiplication * {' '}
+      </button>{' '}
+      </div>
+      <div>
+        <br></br>
+      <button onClick={() => dispatch({ type: 'pow' })}>
+        {' '}
+       Power ^ {' '}
+      </button>{' '}
+      <button type="reset" onClick={() => dispatch({ type: 'reset' })}> Reset </button>{' '}
+      <br /> <br /><br /> <br />
+      </div>
+      Result: {state.count} <br /> <br />
+      &nbsp;
+</form>
+     
     </div>
   );
 }
